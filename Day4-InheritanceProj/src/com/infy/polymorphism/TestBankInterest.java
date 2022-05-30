8fa9e86d3bdb47fdbe493111f204e6a9e8f25f2f
ratnappatel <ratnappatel@gmail.com>
2022-05-30 09:36:50 +0530

    Inheritance

--- Day4-InheritanceProj/src/com/infy/polymorphism/TestBankInterest.java
+++ Day4-InheritanceProj/src/com/infy/polymorphism/TestBankInterest.java
@@ -4,10 +4,13 @@ public class TestBankInterest {
 
 	public static void main(String[] args) {
 
+		// R-Var pointing to an object will be choosen to call overriden method definition
 		RBIBank account=new RBIBank(2500000.00);
 		account.calInterestQuaterly();
+		
 		account=new ICICIBank(2500000.00);
 		account.calInterestQuaterly();
+		
 		account=new BOBBank(2500000.00);
 		account.calInterestQuaterly();
 	}
